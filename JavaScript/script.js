74f1f0e23f001c618053793f8359cf774d50e57b
a-pompom <aoi.matsuda.3x@gmail.com>
2019-04-28 14:52:01 +0900

    リファクタリング

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,94 +1,153 @@
-var todo = (function(){
-	var todoList = [];
-	var todoListHTML = '';
-	var indexList = {};
+//クロージャ定義
+let todo = (function(){
+	//変数定義
+	//スコープチェインにより、関数の内側からも参照可能
+	let todoList = [];
+	//HTML要素の子要素に動的に生成したTODO要素を追加することで描画する
+	let todoListElement = document.getElementById('todoList');
 	
+	/**
+	 * 描画処理
+	 * 各タスク及び削除ボタンをHTML要素で作成する
+	 */
 	function draw(){
-		todoListHTML = '';
-		indexList = {};
-		document.getElementById('todoList').innerHTML = '';
-		for (var i=0; i < todoList.length; i++){
-			var todoRow = document.createElement('ul');
+		//初期化
+		todoListElement.innerHTML = '';
+		
+		/*
+		<ul>(行要素)
+			<li>(タスク＋ピン)
+				<ul>
+					<li>(ピン) </li>
+					<li>(タスク) </li>
+				</ul>
+			</li>
+			<li>(削除ボタン) </li>
+		</ul>
+		上記HTML要素をtodoListの要素数だけ生成
+		*/
+		for (let i=0; i < todoList.length; i++){
+			//行要素
+			let todoRow = document.createElement('ul');
 			todoRow.setAttribute('class', 'todo-list__row');
 			
-			var todoSub = document.createElement('li');
-			todoSub.setAttribute('class', 'todo-list--sub');
-			var todoSubList = document.createElement('ul');
-			var todoPinLi = document.createElement('li');
-			var todoPin = document.createElement('i');
-			todoPin.setAttribute('class', 'fas fa-map-pin todo-list__pin');
-			todoPinLi.appendChild(todoPin);
+			//タスク＋ピン
+			todoRow.appendChild(createTask(i));
 			
-			var todoItem = document.createElement('li');
-			todoItem.setAttribute('class', 'todo-list__item');
-			todoItem.textContent = todoList[i];
+			//削除ボタン
+			todoRow.appendChild(createDelButton(i));		
 			
-			todoSubList.appendChild(todoPinLi);
-			todoSubList.appendChild(todoItem);
-			
-			todoSub.appendChild(todoSubList);
-			
-			todoRow.appendChild(todoSub);
-			
-			var delButtonLi = document.createElement('li');
-			var delButton = document.createElement('button');
-			delButton.setAttribute('class', 'button--del');
-			delButton.textContent = 'del'
-			//delButton.setAttribute('onclick', '"todo.delButtonClick(' + i + ')"');
-			delButton.setAttribute('id', 'delButton-' + i);
-			//document.getElementById('delButton-' + i).addEventListener('click', function(){ del() });
-			delButtonLi.appendChild(delButton);
-			
-			todoRow.appendChild(delButtonLi);
-			document.getElementById('todoList').innerHTML += todoRow.innerHTML;
-			indexList[i] = i;
-			console.log(indexList);
-				
+			//既存のHTML要素へ追加することで描画される
+			todoListElement.appendChild(todoRow);
 		}
-		Object.entries(indexList).forEach(([key, value]) => { 
-    		console.log({key, value});
-			document.getElementById('delButton-' + value).onclick = (function(a) {
-				return function(){ del(a);};
-			})(value);	
-		});
 		
-		//var x = document.getElementById('delButton-' + indexList[i]).getAttribute('id');
+		//削除ボタンクリックイベントの登録
+		//ループインデックスはループ終了時にtodoList.lengthとなってしまうので
+		//不変の配列のインデックスをもとにイベントを登録する
+		todoList.forEach((element, index) => {
+			document.getElementById('delButton-' + index).onclick = ()=>{del(index)};
+		});
+	}
+	
+	/**
+	 * タスクを描画するためのHTML要素を生成
+	 * @param Number itemIndex 描画対象のインデックス
+	 * @return Object todoItem タスクのHTML要素
+	 */
+	function createTask(itemIndex){
+		//ピンとタスク(li)
+		let todoItem = document.createElement('li');
+		todoItem.setAttribute('class', 'todo-list--item');
 			
-				
+		//ピンとタスク(ul)
+		let todoItemUL = document.createElement('ul');
+			
+		//ピンを囲うli要素
+		let todoPinLi = document.createElement('li');
+		//ピンを描画するi要素
+		let todoPin = document.createElement('i');
+		todoPin.setAttribute('class', 'fas fa-map-pin todo-list__pin');
+		todoPinLi.appendChild(todoPin);
+			
+		//タスク
+		let task = document.createElement('li');
+		task.setAttribute('class', 'todo-list__task');
+		task.textContent = todoList[itemIndex];
+			
+		//ピンとタスクをulへセット
+		todoItemUL.appendChild(todoPinLi);
+		todoItemUL.appendChild(task);
+		//ulをliの要素としてセット
+		todoItem.appendChild(todoItemUL);
+		
+		return todoItem;
 	}
 	
-	
-	
-	function add(item) {
-		todoList.push(item);
+	/**
+	 * 各行ごとに削除ボタンを生成する
+	 * @param   Number itemIndex 現在行のインデックス
+	 * @returns Object 削除ボタンのHTML要素
+	 */
+	function createDelButton(itemIndex){
+		//削除ボタンを保持するli要素
+		let delButtonLi = document.createElement('li');
+		//削除ボタン
+		let delButton = document.createElement('button');
+		delButton.setAttribute('class', 'button--del');
+		delButton.textContent = 'Del';
+		//クリックイベントを登録できるようIDを動的に生成　
+		delButton.setAttribute('id', 'delButton-' + itemIndex);
+		
+		//li要素にボタンを追加
+		delButtonLi.appendChild(delButton);
+		
+		return delButtonLi;
 	}
 	
+	/**
+	 * 削除ボタンクリック時に呼ばれる処理
+	 * todoListからクリックされた要素のインデックスに該当するものを削除
+	 * @param int delIndex...削除対象のインデックス
+	 */
 	function del(delIndex) {
-		console.log('run');
+		//各要素のインデックスを検索し、パラメータと等しいもののみ除外
 		todoList = todoList.filter((element, index) => index !== delIndex);
+		
+		//削除したものを除いて再描画
 		draw();
 	}
 	
+	//戻り値として渡されるオブジェクト
+	//変数名.関数名で実行可能となる
 	return {
-		addButtonClick: function (){
-			var item = document.getElementById('todoInput').value;
+		/**
+		 * 追加ボタンクリック時に呼ばれる処理
+		 * フォームの入力値をもとにタスクを生成
+		 */
+		addButtonClick (){
+			//フォームの入力値
+			let item = document.getElementById('todoInput').value;
+			//空の場合はエラーとする
+			if (!item) {
+				alert('入力してください');
+				return;
+			}
+			
 			todoList.push(item);
+			//フォームを初期化し、再描画
+			document.getElementById('todoInput').value = '';
 			draw();
 		},
 		
-		delButtonClick: function(index) {
-			console.log('run');
-			todoList.filter(element => element !== index);
-			draw();
-		},
-		
-		init: function() {
-			var addButton = document.getElementById('addButton');	
+		/**
+		 * 初期化処理
+		 * 追加ボタンにイベントを設定
+		 */
+		init () {
+			let addButton = document.getElementById('addButton');	
 			addButton.addEventListener('click', todo.addButtonClick);
 		}
-	
 	}
 })();
 
 todo.init();
-
