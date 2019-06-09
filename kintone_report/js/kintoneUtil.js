4776d7ab2467f242974eac30d5dd0dae91601b59
a-pompom <aoi.matsuda.3x@gmail.com>
2019-06-10 08:48:13 +0900

    JSのコード整理

--- kintone_report/js/kintoneUtil.js
+++ kintone_report/js/kintoneUtil.js
@@ -1,10 +1,14 @@
 /**
- * POSTメソッドでTODOアプリへレコードを追加するリクエストを送信 リクエスト成功時には
+ * POSTメソッドでアプリへレコードを追加するリクエストを送信 リクエスト成功時には
  * リスト更新のためにリロード
  */
 export function sendPost(recordObj){
-	console.log("util add record");
-	console.log(recordObj);
+	//以下の形式でリクエストを送信
+	// url: リクエスト先 bool値はゲストスペースか否かを表す
+	// method: GET, POST, DELETE, PUTのいずれか
+	// app: 送信対象のアプリID
+	// record: 更新・取得対象のレコード JSON形式で記述
+	// TOKEN: CSRF対策用のトークン
 	kintone.api(kintone.api.url('/k/v1/record', true), 'POST', {
 		app: kintone.app.getId(),
 		record: recordObj,
@@ -14,7 +18,18 @@ export function sendPost(recordObj){
 		});
 }
 
+/**
+ * DELETEメソッドでアプリのレコードを削除するリクエストを送信 リクエスト成功時には
+ * リスト更新のためにリロード
+ */
 export function sendDelete() {
+	//以下の形式でリクエストを送信
+	// url: リクエスト先 bool値はゲストスペースか否かを表す 
+	// また削除は一括で行えるので、「records」となる
+	// method: GET, POST, DELETE, PUTのいずれか
+	// app: 送信対象のアプリID
+	// ids: 削除対象のIDリスト
+	// TOKEN: CSRF対策用のトークン
 	kintone.api(kintone.api.url('/k/v1/records', true), 'DELETE', {
 		app: kintone.app.getId(),
 		ids: [index],
--- kintone_report/js/script.js
+++ kintone_report/js/script.js
@@ -6,23 +6,24 @@ import * as kintoneUtil from './kintoneUtil.js';
 (() => {
 	"use strict";
 	
+	//画面表示時に呼ばれる処理
 	kintone.events.on('app.record.index.show', (event) =>{
+		
 		//kintoneから取得するレコード一覧
 		let records = event.records
-		console.log('record0 length is' + records[0].length);
-		console.log('record0 is' + JSON.stringify(records[0]));
+		
 		//Vueインスタンス
 		let appVue = new Vue({
 			el: "#appReport",	
 			
 			data() {
 				return {
-					projectIndex: 0,
-					reportList: records,
+					projectIndex: 0, // 参照中のプロジェクトのインデックス
+					reportList: records, // kintoneのレコード
 					projectCount: 1,
 					report: {
 						title: "",
-						projectList: 
+						projectList: //プロジェクトの情報をJSONのリストで管理 
 							[
 								{
 									id: 1,
@@ -47,22 +48,29 @@ import * as kintoneUtil from './kintoneUtil.js';
 			},
 			
 			methods: {
-				
+				/**
+				 * 実績リストへ行追加を行う
+				 */
 				addAchievementRow(){
+					//現在参照中のプロジェクトへ空の実績オブジェクトを挿入
 					this.report.
 						projectList[this.projectIndex].
 							achievementList.push({achievement: "", status: ""});
 				},
 				
+				/**
+				 * プロジェクトを追加する
+				 */
 				addProject(){
-					console.log('add proj caaled');
+					//追加要素のIDを現在要素の最大値+1とする
 					let maxId = this.report.projectList.reduce((acc, cur)=>{
 						return acc > cur.id ? acc : cur.id;
 					});
 					
+					//空のプロジェクトオブジェクトを生成
 					this.report.projectList.push(
 						{
-							id: maxId,
+							id: maxId + 1,
 							projectName: "",
 							taskName: "",
 							startDate: "",
@@ -77,32 +85,51 @@ import * as kintoneUtil from './kintoneUtil.js';
 						}
 					);
 					
+					//追加したプロジェクトを参照
 					this.projectIndex = this.report.projectList.length + 1;
 				},
 				
+				/**
+				 * プロジェクトを編集する Indexを切り替えることで参照中のプロジェクトを変更する
+				 * @param {Number} projectIndex 参照対象のプロジェクトのインデックス
+				 */
 				editProject(projectIndex) {
 					this.projectIndex = projectIndex;
 				},
 				
+				/**
+				 * プロジェクトを複製する 同じプロジェクトで別作業をやることは頻繁に起こり得るので
+				 * 複製メソッドを実装した
+				 * @param   {Number} projectIndex 参照中のプロジェクトのインデックス
+				 */
 				copyProject(projectIndex) {
-					console.log(projectIndex);
+					// 複製したプロジェクトに振るIDを設定
 					let maxId = this.report.projectList.reduce((acc, cur)=>{
 						return acc > cur.id ? acc : cur.id;
 					});
 					
+					// 複製対象のプロジェクトをディープコピーしてIDを採番することで複製
 					let copyProject = JSON.parse(JSON.stringify(this.report.projectList[projectIndex]));
-					copyProject.id = maxId;
+					copyProject.id = maxId + 1;
 					this.report.projectList.push(
 						copyProject
 					);
 					
+					//複製結果を参照対象とする
 					this.projectIndex = this.report.projectList.length + 1;
 				},
 				
-				delProject(test, projectIndex) {
+				/**
+				 * プロジェクトを削除する
+				 * @param {Number} projectIndex 削除対象のプロジェクトのインデックス
+				 */
+				delProject(projectIndex) {
 					this.report.projectList.splice(projectIndex, 1);
 				},
 				
+				/**
+				 * レコード一覧へ日報を追加する
+				 */
 				addReport(){
 					kintoneUtil.sendPost({
 						title: {value: this.report.title},
